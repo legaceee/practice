980d2da949144c8ff97da8d847f2bb3944e5a485
aman pandey <amanpandey2707@gmail.com>
2026-03-25 01:02:05 +0530

    pause 1

--- apps/http/src/controller/nodeController.ts
+++ apps/http/src/controller/nodeController.ts
@@ -1,6 +1,43 @@
 import { prisma } from "@repo/db";
 import { Request, Response } from "express";
-
+type nodeInput = {
+  type: string;
+  service: string;
+  config: JSON;
+  order: Number;
+};
+function parseNode(body: any): {
+  data?: nodeInput;
+  error?: string;
+} {
+  if (!body || typeof body !== "object") {
+    return { error: "Invalid payload" };
+  }
+  if (body.type === "trigger" || body.type === "action") {
+    const type = body.type;
+  }
+  if (body.type !== "trigger" || body.type === "service") {
+    return { error: "not proper type" };
+  }
+  const service = typeof body.service === "string" ? body.service.trim() : "";
+  if (!service) {
+    return { error: "empty service" };
+  }
+  const config = body.config;
+  if (!config || typeof config !== "object") {
+    return { error: "empty config is not required" };
+  }
+  const order = Number(body.order);
+  if (!order) {
+    return { error: "order should be number" };
+  }
+  return {
+    type,
+    service,
+    config,
+    order,
+  };
+}
 export const nodeCreation = async (req: Request, res: Response) => {
   try {
     const workflowId = parseInt(req.params.workflow as string);
