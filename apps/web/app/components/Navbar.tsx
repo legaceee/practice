d06b9a54c14e447c88deba90ede26e88b50ca23b
aman pandey <amanpandey2707@gmail.com>
2026-04-02 09:33:06 +0530

    hover effect

--- apps/web/app/components/Navbar.tsx
+++ apps/web/app/components/Navbar.tsx
@@ -1,32 +1,46 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="w-full border-b bg-white">
       <div className="max-w-375 mx-auto flex items-center justify-between px-4 py-4">
-        {/* LEFT - LOGO */}
-        <div className="text-xl font-bold">_zapier</div>
+        <div className="flex gap-6">
+          <div className="text-2xl font-bold">_zapier</div>
 
-        {/* MIDDLE - NAV LINKS */}
-        <div className="hidden md:flex items-center gap-6 text-gray-700">
-          <p className="cursor-pointer hover:text-black">Products</p>
-          <p className="cursor-pointer hover:text-black">Solutions</p>
-          <p className="cursor-pointer hover:text-black">Resources</p>
-          <p className="cursor-pointer hover:text-black">Enterprise</p>
-          <p className="cursor-pointer hover:text-black">Pricing</p>
+          {/* MIDDLE - NAV LINKS */}
+          <div className="hidden md:flex items-center gap-6 text-gray-700">
+            <p className="cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
+              Products
+            </p>
+            <p className="cursor-pointer hover:text-black hover:rounded-xs hover:bg-gray-200 p-2">
+              Solutions
+            </p>
+            <p className="cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
+              Resources
+            </p>
+            <p className="cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
+              Enterprise
+            </p>
+            <p className="cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
+              Pricing
+            </p>
+          </div>
         </div>
 
-        {/* RIGHT - ACTIONS */}
         <div className="flex items-center gap-5">
-          <p className="text-gray-600 cursor-pointer hover:text-black">
+          <p className="text-gray-600 cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
             Explore apps
           </p>
-          <p className="text-gray-600 cursor-pointer hover:text-black">
+          <p className="text-gray-600 cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
             Contact sales
           </p>
-          <p className="text-gray-600 cursor-pointer hover:text-black">
-            Log in
-          </p>
+          <Link href={"/signin"}>
+            <p className="text-gray-600 cursor-pointer hover:text-black hover:bg-gray-200 p-2  hover:rounded-xs">
+              Log in
+            </p>
+          </Link>
 
-          <button className="bg-orange-500 text-white px-4 py-2 rounded-full hover:bg-orange-600">
+          <button className="bg-orange-500 text-white px-4 py-2 rounded-full hover:bg-orange-800">
             Sign up
           </button>
         </div>
